8fabd276a2528790d6c2e613a2a9b1c9c39ed366
Ang Xu <angxu1234@gmail.com>
2019-07-18 13:47:15 -0500

    migration file complete

--- data/migrations/20190718123002_recipe_book.js
+++ data/migrations/20190718123002_recipe_book.js
@@ -1,8 +1,64 @@
-
 exports.up = function(knex) {
-  
+	return knex.schema
+		.createTable("recipes", tabl => {
+			tbl.increments();
+			tbl
+				.text("recipe_name")
+				.unique()
+				.notNullable();
+		})
+		.createTable("ingredients", tbl => {
+			tbl.increments();
+			tbl
+				.text("ingredient_name")
+				.unique()
+				.notNullable();
+		})
+		.createTable("recipes_ingredients", tbl => {
+			tbl.increments();
+			tbl
+				.integer("recipe_id")
+				.unsigned()
+				.notNullable()
+				.references("id")
+				.inTable("recipes")
+				.onDelete("CASCADE")
+				.onUpdate("CASCADE");
+			tbl
+				.integer("ingredient_id")
+				.unsigned()
+				.notNullable()
+				.references("id")
+				.inTable("ingredients")
+				.onDelete("CASCADE")
+				.onUpdate("CASCADE");
+			tbl
+				.integer("quantity")
+				.unsigned()
+				.notNullable();
+		})
+		.createTable("steps", tbl => {
+			tbl.increments();
+			tbl
+				.integer("recipe_id")
+				.unsigned()
+				.notNullable()
+				.references("id")
+				.inTable("recipes")
+				.onDelete("CASCADE")
+				.onUpdate("CASCADE");
+			tbl
+				.integer("step_number")
+				.unsigned()
+				.notNullable();
+			tbl.text("step_name").notNullable();
+		});
 };
 
 exports.down = function(knex) {
-  
+	return knex.schema
+		.dropTableIfExists("recipes")
+		.dropTableIfExists("ingredients")
+		.dropTableIfExists("steps")
+		.dropTableIfExists("recipes_ingredients");
 };
